05ede9139d24379febbe0c8fe473fa69d04b7ed3
lindzeycoonradt <coonradt5678@gmail.com>
2026-01-30 14:38:51 -0700

    updated ReadMe and about

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -6,9 +6,10 @@ const About = () => {
             <div className="container text-center d-flex flex-column flex-grow-1">
                 <img src="/logo.svg" alt="logo" width="80" height="60" className="icon align-self-center"></img>
                 <h1 className="mt-4"><span className='logo-txt'>About Let's Eat</span></h1>
-                <p id='about-p'>Welcome to my UVU 4900 Capstone Project! I have created a full stack web application for users to browse restaurants and save them
-                    to user created folders. I used React for the frontend, Python's FastApi for the backend, PostgreSQL for the 
-                    database, and Python/Postman for testing. 
+                <p id='about-p'>Welcome to my project, Let's Eat! I have created a full stack web application for users to browse restaurants and save them
+                    to user created folders. Users can also take note of their review for each saved restaurant. I used React for the frontend, Python's FastApi for the backend, PostgreSQL for the 
+                    database, and Python/Postman for testing. Future work on this project would include the ability to add and interact with friends, edit and delete 
+                    profile information, and the ability to search or filter to more quickly find restaurants that a user has saved.
                 </p>
             </div>
         </MainLayout>
